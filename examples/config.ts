acd882434a1df7768a7bfc9e4f48406b53556168
Denis <carriere.denis@gmail.com>
2020-04-30 08:41:37 -0400

    update example

--- examples/config.ts
+++ examples/config.ts
@@ -3,5 +3,5 @@ import { JsSignatureProvider } from 'eosjs/dist/eosjs-jssig';
 const { TextEncoder, TextDecoder } = require('util');
 
 const signatureProvider = new JsSignatureProvider(["5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3"]);
-export const rpc = new JsonRpc("http://localhost:8888", { fetch: require('node-fetch') });
+export const rpc = new JsonRpc("http://eos.eosn.io", { fetch: require('node-fetch') });
 export const api = new Api({ rpc, signatureProvider, textDecoder: new TextDecoder(), textEncoder: new TextEncoder() });
--- examples/eos.gl.ts
+++ examples/eos.gl.ts
@@ -5,22 +5,22 @@ import { asset, symbol_code } from "eos-common";
 
 
 (async () => {
-    const EOS = await get_balance(rpc, "eosio.token", "eos.gl", "EOS");
-    const FAST = await get_balance(rpc, "fastecoadmin", "eos.gl", "FAST");
+    const base = await get_balance(rpc, "eosio.token", "eos.gl", "EOS");
+    const quote = await get_balance(rpc, "fastecoadmin", "eos.gl", "FAST");
     const GL = await get_balance(rpc, "token.gl", "eos.gl", "GL");
 
     // user inputs
     const quantity = asset("1.0000 EOS");
-    const symcode_out = symbol_code("FAST");
+    const symcode_out = quote.symbol.code();
 
     // contract inputs
     const symcode_mining = symbol_code("GL");
     const settings = await get_settings( rpc );
 
     // calculations
-    const { fee, out } = calculate_rate( quantity, symcode_out, EOS, FAST, settings.fee );
-    const mining = calculate_mining_rewards( fee, symcode_mining, EOS, GL, settings.mining_rewards );
-    const burn = calculate_out( fee, symbol_code("FAST"), EOS, FAST );
+    const { fee, out } = calculate_rate( quantity, symcode_out, base, quote, settings.fee );
+    const mining = calculate_mining_rewards( fee, symcode_mining, base, GL, settings.mining_rewards );
+    const burn = calculate_out( fee, symcode_out, base, quote );
 
     // logs
     console.log("quantity:", quantity.to_string());
