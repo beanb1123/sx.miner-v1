229d4992f5f793803ce1760559d310a798e87eff
Denis <carriere.denis@gmail.com>
2020-05-03 22:42:53 -0400

    update miners

--- miners/index.ts
+++ miners/index.ts
@@ -17,12 +17,14 @@ export const miners: Miners = {
         "EOS-BNT": require("./eos.gl/bancor.v1/EOS-BNT"),
         "BNT-EOSDT": require("./eos.gl/bancor.v1/BNT-EOSDT"),
         "EOSDT-BNT": require("./eos.gl/bancor.v1/EOSDT-BNT"),
-        "BNT-USDT": require("./eos.gl/bancor.v1/BNT-USDT"),
-        "USDT-BNT": require("./eos.gl/bancor.v1/USDT-BNT")
+        // "BNT-USDT": require("./eos.gl/bancor.v1/BNT-USDT"),
+        // "USDT-BNT": require("./eos.gl/bancor.v1/USDT-BNT")
     },
     "bancor.v2": {
         "BNT-EOS": require("./eos.gl/bancor.v2/BNT-EOS"),
         "EOS-BNT": require("./eos.gl/bancor.v2/EOS-BNT"),
+        // "USDT-BNT": require("./eos.gl/bancor.v2/USDT-BNT"),
+        // "BNT-USDT": require("./eos.gl/bancor.v2/BNT-USDT"),
     },
     "newdex": {
         "EOS-EOSDT": require("./eos.gl/newdex/EOS-EOSDT"),
@@ -30,10 +32,10 @@ export const miners: Miners = {
         "EOSDT-EOS": require("./eos.gl/newdex/EOSDT-EOS"),
         "FAST-EOS": require("./eos.gl/newdex/FAST-EOS"),
     },
-    "stable.sx": {
-        "EOSDT-USDT": require("./eos.gl/stable.sx/EOSDT-USDT"),
-        "USDT-EOSDT": require("./eos.gl/stable.sx/USDT-EOSDT")
-    },
+    // "stable.sx": {
+    //     "EOSDT-USDT": require("./eos.gl/stable.sx/EOSDT-USDT"),
+    //     "USDT-EOSDT": require("./eos.gl/stable.sx/USDT-EOSDT")
+    // },
     "yolo": {
         "BNT-EOS": require("./eos.gl/yolo/BNT-EOS"),
         "EOS-BNT": require("./eos.gl/yolo/EOS-BNT"),
---
+++ plugins/swap.newdex/actions.ts
@@ -0,0 +1,7 @@
+import { transfer } from "../eosio.token/actions";
+import { Name, Asset, name } from "eos-common";
+
+export function buymarket( from: Name, contract: Name, quantity: Asset, code = "eosio.token-eos-eosdt" ) {
+    const memo = `{"type":"sell-market","symbol":"${code}","price":"0.0000","channel":"sx"}`;
+    return transfer(from, name("newdexpublic"), contract, quantity, memo )
+}
