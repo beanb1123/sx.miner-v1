d8a25e5bd44601e3c9f6ebf5999fa453af49614e
Denis <carriere.denis@gmail.com>
2020-05-08 14:22:39 -0400

    add log

--- index.ts
+++ index.ts
@@ -7,6 +7,7 @@ new CronJob('*/2 * * * *', async () => {
     for ( const exchange of Object.keys( miners )) {
         for ( const pair of Object.keys( miners[ exchange ]) ) {
             while ( true ) {
+                console.log(exchange, pair)
                 try {
                     const trx_id = await miners[exchange][pair].mine(ACCOUNT);
                     if ( !trx_id ) break;
