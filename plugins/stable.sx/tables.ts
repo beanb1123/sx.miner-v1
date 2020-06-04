87ed1a97b9fd191e7b0ce7125e7dd02deef7cc64
Denis <carriere.denis@gmail.com>
2020-06-04 17:18:02 -0400

    upgrade

--- plugins/stable.sx/tables.ts
+++ plugins/stable.sx/tables.ts
@@ -2,10 +2,8 @@ import { Asset, SymbolCode } from "eos-common";
 import { rpc } from "../../src/config"
 import * as sx from "sxjs";
 
-export async function get_calculate_rate( quantity: Asset, symcode: SymbolCode, pool_fee = 4 ) {
-    const settings = await sx.get_settings(rpc);
+export async function get_calculate_rate( quantity: Asset, symcode: SymbolCode, amplifier = 20, fee = 20 ) {
     const pools = await sx.get_pools(rpc);
 
-    return sx.get_rate( quantity, symcode, pools, settings );
+    return sx.get_rate( quantity, symcode, pools, { amplifier, fee } );
 }
-// `stablestable`
