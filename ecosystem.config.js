afb097bf859fa783e5521035ce133330f94d1dcc
Denis <carriere.denis@gmail.com>
2020-07-14 10:58:59 -0400

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,43 @@
 module.exports = {
-    apps: {
-        name: "sx.miner",
+    apps: [
+    {
+        name: "sx.stable",
         script: "index.ts",
-        args: [],
         autorestart: true,
-        log_date_format : "YYYY-MM-DD HH:mm"
-    }
+        log_date_format : "YYYY-MM-DD HH:mm",
+        env: {
+            MINER: "stable.sx",
+            ACCOUNT: "miner.sx"
+        }
+    },
+    {
+        name: "sx.swap",
+        script: "index.ts",
+        autorestart: true,
+        log_date_format : "YYYY-MM-DD HH:mm",
+        env: {
+            MINER: "swap.sx",
+            ACCOUNT: "miner.sx"
+        }
+    },
+    {
+        name: "sx.vigor",
+        script: "index.ts",
+        autorestart: true,
+        log_date_format : "YYYY-MM-DD HH:mm",
+        env: {
+            MINER: "vigor.sx",
+            ACCOUNT: "miner.sx"
+        }
+    },
+    {
+        name: "sx.eosdt",
+        script: "index.ts",
+        autorestart: true,
+        log_date_format : "YYYY-MM-DD HH:mm",
+        env: {
+            MINER: "eosdt.sx",
+            ACCOUNT: "miner.sx"
+        }
+    }]
 };
