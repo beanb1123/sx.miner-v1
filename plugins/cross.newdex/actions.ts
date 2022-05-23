832e75024acf6fe580f3c6aab26553ca592c9577
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2022-05-24 00:05:53 +0200

    Delete plugins/cross.newdex directory

--- plugins/cross.newdex/actions.ts
+++
@@ -1,7 +0,0 @@
-import { transfer } from "../eosio.token/actions";
-import { Name, Asset, name } from "eos-common";
-
-export function swap( from: Name, contract: Name, quantity: Asset ) {
-    const memo = `eos:${from}`;
-    return transfer(from, name("cross.newdex"), contract, quantity, memo )
-}
