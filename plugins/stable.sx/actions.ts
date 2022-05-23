60146cc9f4138c28d877a4df286ee16403fb2d0b
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2022-05-24 00:04:54 +0200

    Delete plugins/stable.sx directory

--- plugins/stable.sx/actions.ts
+++
@@ -1,6 +0,0 @@
-import { transfer } from "../eosio.token/actions";
-import { Name, Asset, name, SymbolCode } from "eos-common";
-
-export function buymarket( from: Name, contract: Name, quantity: Asset, symcode: SymbolCode) {
-    return transfer(from, name("stable.sx"), contract, quantity, symcode.to_string() )
-}
