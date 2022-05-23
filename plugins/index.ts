31011ad4bf75e1a5f2ab05be919ca666f4cfe1f7
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2022-05-24 00:13:14 +0200

    Update index.ts

--- plugins/index.ts
+++ plugins/index.ts
@@ -1,31 +1,13 @@
 // Bancor
-import * as bancor from "./bancor"
+import * as bancor from "./swap.box"
 
 // Newdex
-import * as newdex from "./newdex"
-import * as swapNewdex from "./swap.newdex"
-import * as crossNewdex from "./cross.newdex"
+import * as newdex from "./alcor"
+import * as swapNewdex from "./alcor.swap"
+import * as crossNewdex from "./cross.alcor"
 
 // Utils
 import * as yolo from "./yolo"
 import * as delphioracle from "./delphioracle"
 import * as flash from "./flash.sx"
 import * as token from "./eosio.token"
-
-// Pizza
-import * as pizzadex from "./pizzadex"
-import * as pizzaswap from "./pizzaswap"
-
-// SX Swap
-import * as swapSx from "./swap.sx"
-import * as stableSx from "./stable.sx"
-import * as crossSx from "./cross.sx"
-import * as vigorSx from "./vigor.sx"
-import * as eosdtSx from "./eosdt.sx"
-import * as gl from "./eos.gl"
-
-export {
-    swapSx, stableSx, crossSx, vigorSx, eosdtSx,
-    swapNewdex, newdex, crossNewdex,
-    bancor, flash, token, gl, yolo, pizzadex, pizzaswap, delphioracle
-}
