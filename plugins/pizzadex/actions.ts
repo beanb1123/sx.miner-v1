85a1525fff76efec5583f907023e029525ec5292
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2022-05-24 00:05:14 +0200

    Delete plugins/pizzadex directory

--- plugins/pizzadex/actions.ts
+++
@@ -1,9 +0,0 @@
-import { transfer } from "../eosio.token/actions";
-import { Name, Asset, name } from "eos-common";
-
-export function buymarket( from: Name, contract: Name, quantity: Asset, pair: string, type: string ) {
-    const memo = `${type}-${pair}-0-web-0`;
-    return transfer(from, name("pzadexcntrct"), contract, quantity, memo )
-}
-
-// "sell-eos2usde-0-web-0"
