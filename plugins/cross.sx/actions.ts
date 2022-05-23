a7a2946b7d6b7a70cbef376fac51d60b307276cd
beanb1123 <103089678+beanb1123@users.noreply.github.com>
2022-05-24 00:06:27 +0200

    Delete plugins/cross.sx directory

--- plugins/cross.sx/actions.ts
+++
@@ -1,6 +0,0 @@
-import { transfer } from "../eosio.token/actions";
-import { Name, Asset, name } from "eos-common";
-
-export function swap( from: Name, contract: Name, quantity: Asset ) {
-    return transfer(from, name("cross.sx"), contract, quantity, "" )
-}
